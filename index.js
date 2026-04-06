37dae9975f78d56957fc21fba84d3925518afb67
Shivam <shivam9818410221@gmail.com>
2026-04-06 23:57:17 +0530

    Like Count tracker and Unlike by same user added

--- index.js
+++ index.js
@@ -22,10 +22,25 @@ app.get('/profile', isLoggedIn , async (req , res)=>{
 
 app.get('/like/:id', isLoggedIn , async (req , res)=>{
      let post = await postModel.findOne({_id: req.params.id}).populate('user');
-     
-    post.like.push(req.user.iserid);
-      await post.save();
-      res.render('profile');
+     if (post.likes.indexOf(req.user.userid)=== -1){
+        post.likes.push(req.user.userid); 
+      }
+      else{
+      post.likes.splice(post.likes.indexOf(req.user.userid), 1);
+      }
+       await post.save();
+      res.redirect('/profile');
+});
+
+app.get('/edit/:id', isLoggedIn , async (req , res)=>{
+     let post = await postModel.findOne({_id: req.params.id}).populate('user');
+    
+     res.render('edit', {post})
+});
+
+app.post('/update/:id', isLoggedIn , async (req , res)=>{
+     let post = await postModel.findOneAndUpdate({_id: req.params.id}, {content: req.body.content});
+    res.redirect('/profile');
 });
 
 app.post('/post', isLoggedIn , async (req , res)=>{
@@ -80,10 +95,8 @@ app.post('/login' , async (req , res) =>{
    });
 }); 
 
-app.post('/Delete', isLoggedIn, async (req , res) => {
+app.post('/delete', isLoggedIn, async (req , res) => {
    let {postid} = req.body;
-
-   
     await postModel.findByIdAndDelete(postid);
        let user = await userModel.findOne({email: req.user.email});
         user.posts.pull(postid);
