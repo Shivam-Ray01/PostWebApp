21e319bc43e2f570d7e9afe209f59b9c3e48b81d
Shivam <shivam9818410221@gmail.com>
2026-04-10 23:59:08 +0530

    Added multer to store images

--- index.js
+++ index.js
@@ -5,15 +5,28 @@ const postModel = require('./models/post');
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const multerconfig = require('./config/multerconfig');
+const multer = require('./config/multerconfig');
+const path = require('path');
 
 app.set('view engine' , 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
 
 app.get('/' , (req , res) =>{
     res.render('login'); 
+});
+
+app.get('/profile/upload' , (req , res) =>{
+    res.render('profileupload'); 
+});
+
+app.post('/upload' , isLoggedIn, upload.single('image'), async (req , res) =>{
+   let user = await userModel.findOne({email: req.user.email});
+   user.profilepic = req.file.filename;
+   await user.save();
+   res.redirect('/profile');
 });
 
 app.get('/profile', isLoggedIn , async (req , res)=>{
